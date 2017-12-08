6f15233459345228d9621857edc27cc2fb65650e
William Peregoy <williamperegoy@gmail.com>
2017-12-08 12:40:49 -0800

    There are too many conditionals in our Bottles class, and the methods that contain conditionals all take the same argument: "number", we should look to abstract this away into new object - thus, we create class "BottleNumber" (5.8)

---
+++ 99 Bottles of OOP/BottleNumber.swift
@@ -0,0 +1,13 @@
+//
+//  BottleNumber.swift
+//  99 Bottles of OOP
+//
+//  Created by William Peregoy on 12/8/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+import Foundation
+
+class BottleNumber {
+    
+}
