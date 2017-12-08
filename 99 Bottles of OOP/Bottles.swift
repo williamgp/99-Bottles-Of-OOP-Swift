5d9c0773846aaf5ae10f897eb1e9ef42323f1af2
William Peregoy <williamperegoy@gmail.com>
2017-12-08 10:47:01 -0800

    Next difference is "bottle" vs "bottles" this is previously identified concept - "container", abstract to make first phrases identical  (4.4)

--- 99 Bottles of OOP/Bottles.swift
+++ 99 Bottles of OOP/Bottles.swift
@@ -25,12 +25,12 @@ class Bottles {
                 "Go to the store and buy some more, " +
             "99 bottles of beer on the wall.\n"
         case 1:
-            return "\(number) bottle of beer on the wall, " +
+            return "\(number) \(container(number)) of beer on the wall, " +
                 "1 bottle of beer.\n" +
                 "Take it down and pass it around, " +
             "no more bottles of beer on the wall.\n"
         default:
-            return "\(number) bottles of beer on the wall, " +
+            return "\(number) \(container(number)) of beer on the wall, " +
                 "\(number) bottles of beer.\n" +
                 "Take one down and pass it around, " +
             "\(number - 1) \(container(number - 1)) of beer on the wall.\n"
