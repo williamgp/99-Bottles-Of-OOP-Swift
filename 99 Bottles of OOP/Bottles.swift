e49192189660b901fababf0cf1259a33e0e9230f
William Peregoy <williamperegoy@gmail.com>
2017-12-08 12:31:22 -0800

    Test code for all verses without using switch statement. Tests pass. (4.60)

--- 99 Bottles of OOP/Bottles.swift
+++ 99 Bottles of OOP/Bottles.swift
@@ -18,18 +18,22 @@ class Bottles {
     }
     
     func verse(_ number: Int) -> String {
-        switch number {
-        case 0:
-            return "\(quantity(number).capitalizedFirst) \(container(number)) of beer on the wall, " +
-                "\(quantity(number)) \(container(number)) of beer.\n" +
-                "\(action(number)), " +
-            "\(quantity(successor(number))) \(container(number - 1)) of beer on the wall.\n"
-        default:
-            return "\(quantity(number).capitalizedFirst) \(container(number)) of beer on the wall, " +
-                "\(quantity(number)) \(container(number)) of beer.\n" +
-                "\(action(number)), " +
-            "\(quantity(successor(number))) \(container(number - 1)) of beer on the wall.\n"
-        }
+//        switch number {
+//        case 0:
+//            return "\(quantity(number).capitalizedFirst) \(container(number)) of beer on the wall, " +
+//                "\(quantity(number)) \(container(number)) of beer.\n" +
+//                "\(action(number)), " +
+//            "\(quantity(successor(number))) \(container(number - 1)) of beer on the wall.\n"
+//        default:
+//            return "\(quantity(number).capitalizedFirst) \(container(number)) of beer on the wall, " +
+//                "\(quantity(number)) \(container(number)) of beer.\n" +
+//                "\(action(number)), " +
+//            "\(quantity(successor(number))) \(container(number - 1)) of beer on the wall.\n"
+//        }
+        return "\(quantity(number).capitalizedFirst) \(container(number)) of beer on the wall, " +
+            "\(quantity(number)) \(container(number)) of beer.\n" +
+            "\(action(number)), " +
+        "\(quantity(successor(number))) \(container(number - 1)) of beer on the wall.\n"
     }
     
     func container(_ number: Int) -> String {
