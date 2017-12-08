05ebb0608e81c473645de5fc7a5b1936deb32393
William Peregoy <williamperegoy@gmail.com>
2017-12-07 16:26:32 -0800

    Verse 2 Test (2.7)
    [_9_Bottles_of_OOPTests.BottlesTests testVerse2] : XCTAssertEqual failed: ("2 bottles of beer on the wall, 2 bottles of beer.
    Take one down and pass it around, 1 bottle of beer on the wall.
    ") is not equal to ("3 bottles of beer on the wall, 3 bottles of beer.
    Take one down and pass it around, 2 bottles of beer on the wall.
    ") -
    Test Case '-[_9_Bottles_of_OOPTests.BottlesTests testVerse2]' failed (0.036 seconds).

--- 99 Bottles of OOPTests/BottlesTests.swift
+++ 99 Bottles of OOPTests/BottlesTests.swift
@@ -24,4 +24,12 @@ class BottlesTests: XCTestCase {
         "2 bottles of beer on the wall.\n"
         XCTAssertEqual(expected, Bottles().verse(3))
     }
+    
+    func testVerse2() {
+        let expected = "2 bottles of beer on the wall, " +
+            "2 bottles of beer.\n" +
+            "Take one down and pass it around, " +
+        "1 bottle of beer on the wall.\n"
+        XCTAssertEqual(expected, Bottles().verse(3))
+    }
 }
