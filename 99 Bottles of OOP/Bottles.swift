c0158781b0cfc9131138b2694e4e9587396c310a
William Peregoy <williamperegoy@gmail.com>
2017-12-07 16:02:47 -0800

    TDD: step one - define class Bottles

---
+++ 99 Bottles of OOP/Bottles.swift
@@ -0,0 +1,12 @@
+//
+//  Bottles.swift
+//  99 Bottles of OOP
+//
+//  Created by William Peregoy on 12/7/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+import Foundation
+
+class Bottles {
+}
