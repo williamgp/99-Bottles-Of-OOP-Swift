5f2652047180f78cc5d46a8dd3ba33aaef35c3ba
William Peregoy <williamperegoy@gmail.com>
2017-12-07 16:18:54 -0800

    (2.4) Conditional to pass the test

--- 99 Bottles of OOP/Bottles.swift
+++ 99 Bottles of OOP/Bottles.swift
@@ -9,10 +9,17 @@
 import Foundation
 
 class Bottles {
-    func verse(_: Int) -> String {
-        return "99 bottles of beer on the wall, " +
-            "99 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-        "98 bottles of beer on the wall.\n"
+    func verse(_ number: Int) -> String {
+        if number == 99 {
+            return "99 bottles of beer on the wall, " +
+                "99 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+            "98 bottles of beer on the wall.\n"
+        } else {
+            return "3 bottles of beer on the wall, " +
+                "3 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+            "2 bottles of beer on the wall.\n"
+        }
     }
 }
