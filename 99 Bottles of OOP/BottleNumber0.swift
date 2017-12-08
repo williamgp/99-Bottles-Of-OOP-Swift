f8f7e924d9b98c69232765713717e638e3017d43
William Peregoy <williamperegoy@gmail.com>
2017-12-08 15:41:50 -0800

    Time to begin "Replace conditionals with Polymorphism" strategy and create a new BottleNumber0 class to handle the special case when a BottleNumber has a value of 0. (6.11)

---
+++ 99 Bottles of OOP/BottleNumber0.swift
@@ -0,0 +1,13 @@
+//
+//  BottleNumber0.swift
+//  99 Bottles of OOP
+//
+//  Created by William Peregoy on 12/8/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+import Foundation
+
+class BottleNumber0: BottleNumber {
+    
+}
