a47b624dab5a8ad0153c4b8ef751d41e3061e694
William Peregoy <williamperegoy@gmail.com>
2017-12-08 22:32:42 -0800

    adding BottleNumber6 class (6.44)

---
+++ 99 Bottles of OOP/BottleNumber6.swift
@@ -0,0 +1,13 @@
+//
+//  BottleNumber6.swift
+//  99 Bottles of OOP
+//
+//  Created by William Peregoy on 12/8/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+import Foundation
+
+class BottleNumber6: BottleNumber {
+    
+}
