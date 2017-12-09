45f735d2ab7ecdc70ff67a58fbacb854fc044a7b
William Peregoy <williamperegoy@gmail.com>
2017-12-08 18:38:35 -0800

    The next task is to repeat this for 1. Create the BottleNumber1 class. (6.23)

---
+++ 99 Bottles of OOP/BottleNumber1.swift
@@ -0,0 +1,12 @@
+//
+//  BottleNumber1.swift
+//  99 Bottles of OOP
+//
+//  Created by William Peregoy on 12/8/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+import Foundation
+
+class BottleNumber1: BottleNumber {
+}
